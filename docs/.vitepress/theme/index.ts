390f6a722ff183b82c163544fe0552a224887aa4
刘佳鹏 <jaguarliu@tencent.com>
2023-10-20 17:14:11 +0800

    引入documate,为网站引入 ai 能力

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,18 @@
 import BlogTheme from '@sugarat/theme'
+import { h } from 'vue'
+import Documate from '@documate/vue'
+import '@documate/vue/dist/style.css'
 
 // 自定义样式重载
 // import './style.scss'
 
 // 自定义主题色
 // import './user-theme.css'
-
-export default BlogTheme
+export default {
+    ...BlogTheme,
+    Layout: h(BlogTheme.Layout, null, {
+      'nav-bar-content-before': () => h(Documate, {
+        endpoint: 'https://kyq3snf3am.us.aircode.run/ask',
+      }),
+    }),
+  }
